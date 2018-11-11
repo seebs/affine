1a1e0333d5a7eef348aafc237901eee78094c8e1
Seebs <seebs@seebs.net>
2018-11-11 13:32:11 -0600

    add test cases for pre-boxed interfaces

--- README.md
+++ README.md
@@ -6,9 +6,11 @@ performance of several options.
 
 Results:
 
-    AffineTypeSwitch-8  10000  138034 ns/op       0 B/op      0 allocs/op
-    AffineInterface-8    2000  773645 ns/op  240001 B/op  30000 allocs/op
-    AffineConcrete-8   200000   11306 ns/op       0 B/op      0 allocs/op
+    AffineTypeSwitch-8     10000  141219 ns/op       0 B/op      0 allocs/op
+    AffineInterface-8       2000  785927 ns/op  240001 B/op  30000 allocs/op
+    AffineTypeSwitchPre-8  10000  154993 ns/op       0 B/op      0 allocs/op
+    AffineInterfacePre-8    5000  317816 ns/op       0 B/op      0 allocs/op
+    AffineConcrete-8      200000   10532 ns/op       0 B/op      0 allocs/op
 
 So even when they don't require an allocation, interfaces can have a
 very significant performance penalty. Also, using an interface's
--- affine_test.go
+++ affine_test.go
@@ -7,13 +7,21 @@ import (
 var (
 	points  []Point
 	moves   []Move
+	pVecs   []Vec
+	mVecs   []Vec
+	cVecs   []Vec
 	affines []Affine
 )
 
 func init() {
 	for i := 0; i < 10000; i++ {
-		points = append(points, Point{X: float32(i), Y: float32(i)})
-		moves = append(moves, Move{X: float32(i), Y: float32(i)})
+		p := Point{X: float32(i), Y: float32(i)}
+		m := Move{X: float32(i), Y: float32(i)}
+		points = append(points, p)
+		moves = append(moves, m)
+		pVecs = append(pVecs, p)
+		mVecs = append(mVecs, m)
+		cVecs = append(cVecs, p.Add(m))
 		affines = append(affines, Affine{A: 2, B: 2, E: float32(i), F: float32(i)})
 	}
 }
@@ -34,6 +42,30 @@ func BenchmarkAffineInterface(b *testing.B) {
 			_, _ = affines[idx].PVecInterface(points[idx])
 			_, _ = affines[idx].PVecInterface(moves[idx])
 			_, _ = affines[idx].PVecInterface(points[idx].Add(moves[idx]))
+		}
+	}
+}
+
+func BenchmarkAffineTypeSwitchPre(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for idx := range points {
+			_, _ = affines[idx].PVec(pVecs[idx])
+			_, _ = affines[idx].PVec(mVecs[idx])
+			// do the computations, then use the precomputed interface
+			_ = points[idx].Add(moves[idx])
+			_, _ = affines[idx].PVec(cVecs[idx])
+		}
+	}
+}
+
+func BenchmarkAffineInterfacePre(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for idx := range points {
+			_, _ = affines[idx].PVecInterface(pVecs[idx])
+			_, _ = affines[idx].PVecInterface(mVecs[idx])
+			// do the computations, then use the precomputed interface
+			_ = points[idx].Add(moves[idx])
+			_, _ = affines[idx].PVecInterface(cVecs[idx])
 		}
 	}
 }
